a855e46f9ed5210244f6343c76175d61506788bf
Marcus Herstik <marcus@herstik.com>
2023-08-30 16:04:20 +1000

    Update Wk8.py

--- Wk8.py
+++ Wk8.py
@@ -17,9 +17,14 @@ comp_choice = r.randrange(1,101)
 print(comp_choice)
 
 # get input from user
-user_choice = input("Gimme a number, punk: ")
-# check if it's really a number
+def getChoice():
+    global user_choice
+    user_choice = input("Gimme a number, punk: ")
+    return user_choice
 
+user_choice = getChoice()
+
+# check if it's really a number
 def checkDigit():
     global user_choice
     if user_choice.isdigit():
@@ -32,13 +37,17 @@ def checkDigit():
         return False
 
 while checkDigit() == False:
-    user_choice = input("Gimme a number, punk: ")
-
-
-if user_choice == comp_choice:
-    print("Yay !!\n You won")
-else:
-    print("You got it wrong!!")
+    #user_choice = input("Gimme a number, punk: ")
+    getChoice()
+    
+# Loop for the game
+while chances > 0:
+    if user_choice == comp_choice:
+        print("Yay !!\nYou won")
+    else:
+        print("You got it wrong!!")
+        chances -= 1  
+        getChoice()
 
 
 
