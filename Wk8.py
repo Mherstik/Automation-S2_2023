04aa0fa642eba795d340d15d2c905867749f9470
Marcus Herstik <marcus@herstik.com>
2023-08-30 17:40:09 +1000

    Update Wk8.py

--- Wk8.py
+++ Wk8.py
@@ -12,46 +12,55 @@ import random as r
 chances = 3
 
 ## program picks a number between 1 and 100
-
 comp_choice = r.randrange(1,101)
 print(comp_choice)
 
-# get input from user
-def getChoice():
-    global user_choice
-    user_choice = input("Gimme a number, punk: ")
-    return user_choice
 
-user_choice = getChoice()
-
-# check if it's really a number
+# get a number and check if it's really a number
 def checkDigit():
     global user_choice
     if user_choice.isdigit():
         user_choice = int(user_choice)
-        return True
+        if user_choice not in range(1,100):
+            print("Outside of range 1 to 100")
+            getChoice()
+        return True 
+        #return user_choice
         # do something
     else: 
         # ask for a number
         print("That's not a number")
+        getChoice()
         return False
+   
+# get input from user
+def getChoice():
+    global user_choice
+    user_choice = input("Gimme a number between 1 and 100, punk: ")
+    checkDigit()
 
-while checkDigit() == False:
-    #user_choice = input("Gimme a number, punk: ")
-    getChoice()
-    
+#user_choice = getChoice()
+
 # Loop for the game
 while chances > 0:
+    getChoice()
+#    global user_choice
+#    global comp_choice
+    chances -= 1
     if user_choice == comp_choice:
         print("Yay !!\nYou won")
+        break
     else:
         print("You got it wrong!!")
-        chances -= 1  
-        getChoice()
-
-
-
-
+        if user_choice > comp_choice:
+            print("Too High")
+        else:
+            print("Too low")
+    print("You have", chances, "left")
+    # Check high or low
+    #getChoice()
+    
+    
 
 
 
