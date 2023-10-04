cd479cc891e53c8f98255ec1270b7535f606a420
Marcus Herstik <marcus@herstik.com>
2023-10-04 16:04:58 +1000

    Update T4Wk1.py

--- T4Wk1.py
+++ T4Wk1.py
@@ -20,10 +20,31 @@ BONUS 2: create a menu that they choose from of 4,6,10,12,20.
 # Roll the dice!!!
 
 import random
-
-def dice_roll():
-    return random.randint(1, 6)
     
+def num_choice():
+    global user_dice 
+    user_dice = int(input("How many sides do you want to roll: "))
+
+def dice_roll(sides):
+    return random.randint(1, sides)
+
 #print(random.randint(1, 6))
 
-print("You rolled a", dice_roll())
+if __name__ == '__main__':
+    global user_dice
+    started = False
+    num_choice()
+    while True:
+        if started == True:
+            start = input("Do you want to roll dice again: (Y/n): ")
+            if start.upper() == "Y" or start.upper() == "YES":
+                choose_dice = input("Do you want to roll the same (Y/n): ")
+                if choose_dice.upper() == "N" or choose_dice.upper() == "NO": 
+                    num_choice()
+            else:
+                #continue
+                break
+     
+        print("You rolled a", dice_roll(user_dice))
+        started = True
+    print("Thanks for using DICE ROLLER!!")
