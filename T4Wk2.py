6b9bd60034833c7a6c1e36448cc3064ba862bd7b
Marcus Herstik <marcus@herstik.com>
2023-10-11 15:46:46 +1000

    Update T4Wk2

--- T4Wk2.py
+++ T4Wk2.py
@@ -14,4 +14,18 @@ Created on Wed Oct 11 14:04:36 2023
 # 
 # 
 
+def count_characters(text):
+    char_frequency = {}
+    for char in text:
+        if char.isalpha():
+           # char = char.lower()
+            char_frequency[char] = char_frequency.get(char, 0) + 1
+        elif char.isspace():
+            char_frequency["SPACE"] = char_frequency.get("SPACE",0) + 1
+    return char_frequency
 
+user_input = input("Enter a string: ")
+frequency = count_characters(user_input)
+print("Character frequencies:")
+for char, count in frequency.items():
+    print(f"'{char}': {count}")
