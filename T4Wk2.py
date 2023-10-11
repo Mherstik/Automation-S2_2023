ae3f87c82d13d2956d8c1b88a124c7f0d635a3d3
Marcus Herstik <marcus@herstik.com>
2023-10-11 14:56:43 +1000

    Create T4Wk2.py

---
+++ T4Wk2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 14:04:36 2023
+
+@author: marcus
+"""
+
+# Get string (from file or user)
+# start with a variable called char_freq
+# if character is in list, add 1
+# else, add character to list with value 1
+# print list of characters and frequency at the end
+# 
+# 
+
+
